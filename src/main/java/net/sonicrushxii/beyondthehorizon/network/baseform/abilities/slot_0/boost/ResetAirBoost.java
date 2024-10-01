222c343d72ef62a573e55279b723e66e67d72e18
My Laptop <jonat146@gmail.com>
2024-10-01 09:11:06 +0530

    Added Air Boost Mechanic

--- src/main/java/net/sonicrushxii/beyondthehorizon/network/baseform/abilities/slot_0/boost/ResetAirBoost.java
+++ src/main/java/net/sonicrushxii/beyondthehorizon/network/baseform/abilities/slot_0/boost/ResetAirBoost.java
@@ -1,23 +1,12 @@
 package net.sonicrushxii.beyondthehorizon.network.baseform.abilities.slot_0.boost;
 
-import net.minecraft.core.particles.DustParticleOptions;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.network.protocol.game.ClientboundSetEntityMotionPacket;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.network.CustomPayloadEvent;
 import net.sonicrushxii.beyondthehorizon.capabilities.PlayerSonicFormProvider;
 import net.sonicrushxii.beyondthehorizon.capabilities.baseform.BaseformProperties;
-import net.sonicrushxii.beyondthehorizon.modded.ModSounds;
 import net.sonicrushxii.beyondthehorizon.network.PacketHandler;
-import net.sonicrushxii.beyondthehorizon.network.sync.ParticleAuraPacketS2C;
-import net.sonicrushxii.beyondthehorizon.network.sync.ParticleRaycastPacketS2C;
 import net.sonicrushxii.beyondthehorizon.network.sync.SyncPlayerFormS2C;
-import org.joml.Vector3f;
 
 public class ResetAirBoost {
 
